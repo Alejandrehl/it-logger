87e09bdf0519f3a290c0c65faa024258c1744819
Alejandro Hernández <Alejandrehl@icloud.com>
2019-12-31 18:58:08 -0300

    avances add log modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -19,6 +20,7 @@ const App = () => {
       <div className="container">
         <Logs />
         <AddBtn />
+        <AddLogModal />
       </div>
     </div>
   );
