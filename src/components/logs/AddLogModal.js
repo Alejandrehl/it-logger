4b287f0f357dd7171c0ec9fb54b3d1f3ce991c8e
Alejandro Hernández <Alejandrehl@icloud.com>
2019-12-31 19:07:34 -0300

    Launch toast

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
 
-  const onSubmit = () => console.log("On Submit");
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Please enter a message and tech" });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
 
   return (
     <div id="add-log-modal" className="modal" style={modalStyle}>
