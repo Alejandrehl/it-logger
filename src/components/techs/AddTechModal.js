cb5d94f0c1bf4bc1713c9277bf219c4ca8eda6b0
Alejandro Hernández <Alejandrehl@icloud.com>
2019-12-31 19:23:31 -0300

    Add tech modal

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddTechModal = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastname] = useState("");
+
+  const onSubmit = () => {
+    if (firstName === "" || lastName === "") {
+      M.toast({ html: "Please enter a first name and last name" });
+    } else {
+      console.log(firstName, lastName);
+      // Clean Fields
+      setFirstName("");
+      setLastname("");
+    }
+  };
+
+  return (
+    <div id="tech-modal" className="modal" style={modalStyle}>
+      <div className="modal-content">
+        <h4>Enter System Log</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <label htmlFor="firstName" className="active">
+              First Name
+            </label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={e => setLastname(e.target.value)}
+            />
+            <label htmlFor="lastName" className="active">
+              Last Name
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect blue btn"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%"
+};
+
+export default AddTechModal;
