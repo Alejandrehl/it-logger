69925bd1ddacd3e90a565f88c8f94522d98108bf
Alejandro Hernández <Alejandrehl@icloud.com>
2019-12-31 00:24:45 -0300

    Logs component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -12,9 +13,12 @@ const App = () => {
   });
 
   return (
-    <Fragment className="App">
+    <div className="App">
       <SearchBar />
-    </Fragment>
+      <div className="container">
+        <Logs />
+      </div>
+    </div>
   );
 };
 
