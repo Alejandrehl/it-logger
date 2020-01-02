c12214255eb3c4426ccf64aa2858a2682deed4e4
Alejandro Hernández <Alejandrehl@icloud.com>
2020-01-02 17:44:31 -0300

    Redux store and provider setup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -18,17 +20,19 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      <SearchBar />
-      <div className="container">
-        <Logs />
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
+    <Provider store={store}>
+      <div className="App">
+        <SearchBar />
+        <div className="container">
+          <Logs />
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
